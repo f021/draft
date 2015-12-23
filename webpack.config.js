b68ebae88ba7d76d1d334ff69c67413f5cc5daae
andrei <andrei.fzto@gmail.com>
2015-12-23 16:53:41 +0300

    module.exports to main

--- webpack.config.js
+++ webpack.config.js
@@ -3,21 +3,24 @@ var webpack = require('webpack');
 
 module.exports = {
   entry: [
-    'babel-polyfill',
-    './src/main',
-    'webpack-dev-server/client?http://localhost:8080'
+    // 'babel-polyfill',
+    './src/main'
+    // 'webpack-dev-server/client?http://localhost:8080'
   ],
   output: {
-      publicPath: '/',
-      filename: 'main.js'
+      path: './src',
+      filename: 'bundle.js',
+      library: 'main'
   },
   debug: true,
   devtool: 'source-map',
   module: {
     loaders: [
-      { 
+      {
         test: /\.js$/,
-        include: path.join(__dirname, 'src'),
+        include: [
+          path.resolve(__dirname, './src')
+        ],
         loader: 'babel-loader',
         query: {
           presets: ['es2015', 'stage-0']
