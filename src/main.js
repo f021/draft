c85846430bdb22f8421ac2555044d7ce7a88334a
andrei <andrei.fzto@gmail.com>
2015-12-26 17:02:26 +0300

    start make calculator for fcc

--- src/main.js
+++ src/main.js
@@ -1,1 +1,21 @@
 console.log('hello world');
+
+const makeBtn = (btn = "") => {
+    for(let i = 9; i >= 0; i--) {
+      btn += `<button>${i}</button>\n`
+    }
+    console.log(btn);
+    return btn;
+}
+
+document.body.innerHTML = `
+<form>
+  <input type="text">
+  ${(btn => {
+    for(let i = 9; i >= 0; i--) {
+      btn += (!(i % 3) ? '</br>' : '') + `<button>${i}</button>`;
+    };
+    return btn;
+  })("")}
+</form>
+`
