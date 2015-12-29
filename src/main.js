47836a3059b09b8958b086af18fd794ee9469626
andrei <andrei.fzto@gmail.com>
2015-12-29 10:36:32 +0300

    pomodoro done

--- src/main.js
+++ src/main.js
@@ -1,29 +1,20 @@
-
-let time = 50;
-const a = () => { setInterval(() => {
-  document.getElementById('time').innerText = time--;
-}, 1000);
-}
-
-const dec = _ => time -= 60;
-const inc = _ => time += 60;
-const Timer = (time) => {
-
-  let timeID;
-
-  return {
-
-    start() {
-      timeID = setInterval(() => {
-      if (document.querySelector('input[name="player"]:checked').value === 'start') {
-        document.querySelector('#time').innerText = time--;
-      };
-      console.log(document.querySelector('input[name="player"]:checked').value);
-      }, 1000);
-    },
-
-    stop() { clearInterval(timeID);}
+const $ = str => document.querySelector(str);
+const time = (elm = $('#time'), attr = "time") => elm.dataset[attr];
+const addZero = n => '0'.repeat(2 - String(n).length) + n;
+const set = n => {
+  if (n >= 0 && n <= 59 * 60) {
+    $('#time').dataset.time = n;
+    $('#time').innerText = `${addZero(Math.floor(time()/60))}:${addZero(time()%60)}`;
   }
 };
+const inc = n => set(+time() + n);
+const reset = _ => {set (25 * 60); $('#pause').checked = true;};
+const done = _ => {alert('done!'); reset(); };
 
-module.exports = {Timer};
+setTimeout(function go() {
+  if ($('#start').checked && time() > 0) inc(-1);
+  if (+time() === 0) done();
+  setTimeout(go, 1000);
+}, 1000);
+
+module.exports = {inc, reset};
