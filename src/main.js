08318fdd25d8e052daaa3b6ec1b7bb085a2f9629
andrei <andrei.fzto@gmail.com>
2015-12-30 22:04:38 +0300

    before upload

--- src/main.js
+++ src/main.js
@@ -16,15 +16,19 @@ arr.filter(obj => { return new RegExp($('#str').value, 'gi').test(obj.headline +
       let elm = document.createElement('li');
       elm.style.backgroundImage = `url(${obj.image || obj.author.picture})`;
       elm.innerHTML = `
+      <div class='headline'>
         <a href='${obj.link}' target='_blank'>
           <h1>${obj.headline}</h1>
         </a>
+      </div>
+      <div class='details'>
         <p>
           <span>♥ ${obj.rank} points</span>
           <span>
             <a href='http://freecodecamp.com/${obj.author.username}' target='_blank'>@${obj.author.username}</a>
           </span>
-        </p>`
+        </p>
+      </div>`
   $('#news').appendChild(elm);
   });
 }
