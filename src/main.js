3c5ed763ba8f00ab64e50c8d0cd71170bc58a4a5
andrei <andrei.fzto@gmail.com>
2016-01-02 17:33:05 +0300

    playing with simon game 4 from codepen

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 const audioUrls = ['http://markjackson.nl/sounds/E4.mp3', 'http://markjackson.nl/sounds/A3.mp3', 'http://markjackson.nl/sounds/Csharp3.mp3', 'http://markjackson.nl/sounds/E3.mp3']
 const audioNodes = audioUrls.map(e => new Audio(e))
 const msgHello = ` hello, i'm Simon, i'm memory trainer! let's begin...      `.split('')
-const msgLose = `   we need more practice...      `.split('')
-const msgWin = `    you're amazing...      `.split('')
+const msgLose = ` we need more practice...      `.split('')
+const msgWin = ` you're amazing...      `.split('')
 const playground = ['#blue', '#red', '#green', '#yellow']
-const sayYes = ['Ok', ':)', 'wow', 'easy']
-const sayNo = ['f**k', ':(', 'ups']
-const WIN = 2
-const TIME = 1000
+const sayYes = ['Ok', ':)', 'wow', 'easy', 'ninja']
+const sayNo = ['f**k', ':(', 'ups' , 'lol']
+const WIN = 20
+const TIME = 500
 
 const $ = str => document.querySelector(str)
 
-const getRandomElm = arr =>
-  arr[Math.floor(Math.random() * arr.length)]
+const getRandomElm = arr => arr[Math.floor(Math.random() * arr.length)]
 
 const generateSequence = (seed, arr = []) =>
   () => {
@@ -21,18 +20,13 @@ const generateSequence = (seed, arr = []) =>
 }
 
 const runingStr = (arr, len) =>
-  arr.slice(1).reduce((arr, elm) => arr.concat(arr[arr.length - 1].slice(-len + 1) + elm)
+  arr.slice(1).reduce((arr, elm) =>
+    arr.concat(arr[arr.length - 1].slice(-len + 1) + elm)
   , [addToLeft(arr[0], ' ', len)])
 
-const setMsg = str => {
-  $('#level').innerText = str
-}
+const setMsg = str => $('#level').innerText = str
 
-const addToLeft = (str, ch, n) =>
-  ch.repeat(n - str.length) + str
-
-// const addZero = n =>
-//   addToLeft(String(n), '0', 2)
+const addToLeft = (str, ch, n) => ch.repeat(n - str.length) + str
 
 const delay = ms => new Promise(
   (resolve, reject) => setTimeout(resolve, ms)
@@ -41,6 +35,7 @@ const delay = ms => new Promise(
 const playON = str => {
   $(str).classList.toggle('off');
   // audioNodes[playground.indexOf(str)].play()
+  new Audio(audioUrls[playground.indexOf(str)]).play()
 }
 
 const playOFF = str => {
@@ -51,18 +46,19 @@ const playOFF = str => {
 
 const click = (str, ms) => new Promise(
   (resolve, reject) => {
+    console.log(ms)
     playON(str)
     delay(ms).then(() => {
       playOFF(str)
-      return delay(ms - (ms / 100) * 20)
+      return delay(100)
     }).then(() => resolve())
   }
 )
 
-const playSequence = (arr, cb) => {
+const playSequence = (arr, ms, cb) => {
   !arr.length
     ? cb()
-    : click(arr[0], TIME - 100 * arr.length).then(() => playSequence(arr.slice(1), cb))
+    : click(arr[0], ms).then(() => playSequence(arr.slice(1), ms, cb))
 }
 
 const say = arr => new Promise(
@@ -72,12 +68,13 @@ const say = arr => new Promise(
         $('#level').classList.remove('text-right')
         resolve();
       } else {
-        delay(150).then(() => {
+        delay(100).then(() => {
           setMsg(arr[0])
           recur(arr.slice(1))})
       }
     }
     $('#level').classList.add('text-right')
+    setMsg('')
     recur(arr)
   }
 )
@@ -96,28 +93,34 @@ const gameOver = _ => {
 
 const listen = seq => new Promise(
   (resolve, reject) => {
+
+    const stop = _ => playground.forEach(e =>
+      $(e).removeEventListener('click', whenClick))
+
     const whenClick = (e, elm = `#${e.target.id}`) => {
-      $(elm).removeEventListener('click', whenClick);
+      // $(elm).removeEventListener('click', whenClick);
       setMsg(addToLeft(String(seq.length - 1), '0', 2))
-      click(elm, 600).then(() => {
+      click(elm, 100).then();
+        // $(elm).addEventListener('click', whenClick)
+        console.log(elm, seq[0])
         if (seq[0] !== elm) {
           setMsg(getRandomElm(sayNo))
-          playground.forEach(e => $(e).removeEventListener('click', whenClick))
+          stop()
           delay(1000).then(reject)
         } else {
           seq.shift();
-          setMsg(addToLeft(String(seq.length), '0', 2))
+          // setMsg(addToLeft(String(seq.length), '0', 2))
         }
         if (seq.length === 0) {
           setMsg(getRandomElm(sayYes))
           delay(1000).then(() => {
-            playground.forEach(e => $(e).removeEventListener('click', whenClick))
+            stop()
             resolve()
           })
         }
-      })
-    $(elm).addEventListener('click', whenClick);
-    }
+      }
+
+
     playground.forEach(e => $(e).addEventListener('click', whenClick))
 })
 
@@ -125,19 +128,21 @@ const go = (getSeq, seq = getSeq()) => {
   setMsg(addToLeft(String(seq.length), '0', 2));
   (seq.length === WIN)
     ? win()
-    : playSequence(seq, () =>
+    : playSequence(seq, TIME - 50 * seq.length, () =>
         listen(seq.slice())
         .then(() => go(getSeq))
         .catch(gameOver))
 }
 
 const startEngine = _ => {
+  audioNodes.forEach((e,index) => delay(index*100).then(()=>e.play()))
   $('#start').removeEventListener('click', startEngine)
   playground.forEach((e) => $(e).classList.remove('off'))
   say(runingStr(msgHello, 6)).then(() => {
     $('#start').addEventListener('click', reset)
     reset()
   })
+  // reset()
 }
 
 const reset = _ => {
@@ -147,6 +152,8 @@ const reset = _ => {
 })
 }
 
+// playSequence(['#blue','#blue','#blue','#blue','#blue','#blue','#blue','#blue']
+// , 200, ()=>console.log('aa'))
 $('#start').addEventListener('click', startEngine)
 
 module.exports = {win, say, runingStr}
