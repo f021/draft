4c6a97560e3cb7ffca92075fc2ff24cc63d103ee
andrei <andrei.fzto@gmail.com>
2016-02-04 16:02:30 +0300

    refactoring rogue

--- src/rogue.js
+++ src/rogue.js
@@ -8,8 +8,16 @@
 
 import { add, sub } from './vector'
 
+// fold :: Function -> [a] -> [a] -> [a]
+const fold = fn => (arr, ...args) =>
+  arr.reduce((acc, x) => {
+    let val = fn(x, ...args)
+    return val ? [...acc, val] : acc
+  }, [])
+
+// compose :: [Function] -> a -> a
 const compose = (...fns) => x =>
-  fns.reduce((result, fn) =>
+  fns.reduceRight((result, fn) =>
     fn(result), x)
 
 const Field = ({ w, h, flag=true }) => {
@@ -23,21 +31,37 @@ const Field = ({ w, h, flag=true }) => {
     }
   }
 
-// {x, y} for plain array
+// return index of array for { x, y }
   const plain = ({ x, y }, i = x + y * w) => {
     if ((x >= 0 && x < w) && (y >= 0 && y < h) && (i < w * h)) {
       return i;
     }
   }
 
+// return { x, y } for plain array
   const xy = i => ({
     x: i % w,
     y: Math.floor(i / w)
   })
 
+  // indexSubIndex :: Function -> Functoin -> Function
+  const indexSubIndex = compose(sub, xy)
+
+  // 
+  const vecPlusIndex = (v, a) => add(v, xy(a))
+
+  const getVectorMap = (arr, s) => compose(fold, addVec)
+  const getIndexMap = (arr, s) => compose(fold, subVec)
+
+  const getVectorMap = (arr, s) =>
+    arr.reduce((acc, x) => x ? [...acc, sub(xy(x), xy(begin))] : acc , [])
+      //  .filter(elm => typeof elm !== 'undefined')
+
+
   return {
-    xy,
-    x: flag ? plain : comose(tor, plain)
+    xy: flag ? xy : tor,
+    x: flag ? plain : compose(plain, tor)
+
   }
 
 }
